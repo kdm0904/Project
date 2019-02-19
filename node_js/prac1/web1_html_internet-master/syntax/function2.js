03dbf6778d54f0e7f58fa5574dab27873c878e28
kdm0904 <45680681+kdm0904@users.noreply.github.com>
2019-02-19 14:59:30 +0900

    Add files via upload

---
+++ node_js/prac1/web1_html_internet-master/syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second) { // 매개변수 parameter
+  return (first+second);
+}
+
+console.log(sum(3, 4)); // 입력값 하나하나를 의미 argument 인자
---
+++ node_js/prac1/web1_html_internet-master/syntax/template_literal.js
@@ -0,0 +1,7 @@
+var name = 'kdm';
+
+var letter = `Dear ${name}
+
+Are you okay? ${name} ${1+1}`;
+
+console.log(letter);
